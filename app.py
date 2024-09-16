7f43d963c274ccbfd2bef526f1a50b7c1bc96fc0
r322563 <ryansilvasanches16@gmail.com>
2024-09-16 08:50:33 -0300

    aula  frontend 01

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import requests
 
 app = Flask(__name__)
 
 API_URL = 'http://127.0.0.1:5000/api/movies'
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    response = requests.get(API_URL)
-    return render_template('index.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        genre = request.form['genre']
+        requests.post(
+            API_URL,
+            json={'title':title, 'genre':genre}
+        )
+        return redirect('/')
+    else:
+        response = requests.get(API_URL)    
+        if response.status_code == 200:
+            movies = response.json()
+        else:
+            movies = []
+    return render_template('index.html', movies=movies)
+
+@app.route('/delete/<int:id>')
+
+def delete(id):
+    requests.delete(f'{API_URL}/{id}')
+    return redirect('/')
+
+@app.route('/<int:id>', methods=['GET', ' POST',]) 
+def update(id):
+    if  request. method == 'post':
+        title = request.form['title']
+        genre = request.form['genre']
+        requests.put(f'{API_URL}/{id}',json={'title': title, 'genre': genre})
+
+        response = requests.get(f'{API_URL}/{id}')
+        if response.status_code == 200:
+            movie = response.json()
+        else:
+            movie = {}
+    return render_template('update.html', movie=movie)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
